ca114b72a2a6420b92e677ad7c47b8fd5debe36f
VildanovN <vildanov.n98@gmail.com>
2022-11-09 18:20:30 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    origin: true,
     allowedHeaders: 'Authorization',
     methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
     credentials: true,
